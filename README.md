cd99a19d385e159f9746356429f7495ff003f45f
Ruslan Ishmametov <russ-i@yandex.ru>
2018-04-01 20:01:52 +0300

    Функция парсинга сайта

--- README.md
+++
@@ -1,2 +0,0 @@
-# php_site_parser
-PHP application for parsing sites and extracting information from them
--- index.php
+++ index.php
@@ -7,8 +7,18 @@
  */
 
 function Parse( $p1, $p2, $p3 ) {
+	$num1 = strpos( $p1, $p2 );
+	if ( $num1 === false ) {
+		return 0;
+	} else {
+		$num2 = substr( $p1, $num1 );
+
+		return strip_tags( substr( $num2, 0, strpos( $num2, $p3 ) ) );
+	}
 
 }
 
-$siteContent = file_get_contents( "https://pixabay.com/" );
-echo $siteContent;
+$siteContent = file_get_contents( "https://www.shokoladki.ru/" );
+
+echo Parse( $siteContent, '<div class="tel">', '<i>Пн.–Пт. 9:00 – 18:00</i>' );
+
