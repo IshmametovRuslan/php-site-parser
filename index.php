b5590dded840e2c822ea2810fc634bd66c69426d
Ruslan Ishmametov <russ-i@yandex.ru>
2018-04-01 19:06:14 +0300

    1

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ruslan
+ * Date: 01.04.2018
+ * Time: 18:25
+ */
+
+function Parse( $p1, $p2, $p3 ) {
+
+}
+
+$siteContent = file_get_contents( "https://pixabay.com/" );
+echo $siteContent;
